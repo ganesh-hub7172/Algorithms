4ff51f38550211b0acf441e4f3e5caaef1278cf9
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-07-10 17:02:36 +0530

    Time: 272 ms (98.52%) | Memory: 0B (100.00%) - LeetSync

---
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT
+    x,
+    y,
+    z,
+    CASE
+        WHEN x + y + z - GREATEST(x, y, z) > GREATEST(x, y, z)
+        THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM Triangle;
